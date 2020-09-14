1dbdaaf273983c7bbf9e327b0f779ca435f108d4
Christian <alvescastro00cca13@gmail.com>
2020-09-14 18:10:55 -0300

    Components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import {soma} from './soma'
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App' 
 
-console.log(soma(1, 10))
+// JSX: HTML dentro do JavaScript (JavaScript XML)
+render(
+   <App/>, document.getElementById('app')
+)
