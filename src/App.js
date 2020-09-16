6f9f5ed686532d105532d53e023ca2d6bf7f8760
Christian <alvescastro00cca13@gmail.com>
2020-09-16 12:23:22 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 
 
 function App() {
+    const [projects, setProjects] = useState(["Back-end", "Front-end"])
+
+    //useState retorna um array com duas posições: 
+    // 1 - Variável com seu valor inicial
+    // 2 - Uma função para atualizar esse valor
+
+    function handleAddProject(){
+        // projects.push(`New data${Date.now()}`)
+
+        setProjects([...projects, `New data${Date.now()}`])
+
+        console.log(projects)
+    }
+
     return (
         <>
-            <Header title="ReactJs">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                </ul>
-            </Header>
-            <Header title="NodeJs">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                    <li>Login</li>
-                </ul>
-            </Header>
+            <Header title="Projects"/>
+
+            <ul>
+                {projects.map(project => (
+                    <li key={project}>{project}</li>
+                    //quando é feita uma iteração no react, que percorrido um vetor 
+                    //o react precisa que seja identificado no primeiro elemento, 
+                    //uma propriedade chamada key. Aí deverá ser informado a informação 
+                    //que é unica dentro de cada dado no array 
+                ))} 
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
 
 
         </>
